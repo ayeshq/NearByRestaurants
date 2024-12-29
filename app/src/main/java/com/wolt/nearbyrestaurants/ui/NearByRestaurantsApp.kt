2d533099b55ce9dd758325836f316b166636598f
Ayesh <ayeshqumhieh@gmail.com>
2024-12-29 15:03:15 +0200

    Use Surface as the root for optimal color contrast

--- app/src/main/java/com/wolt/nearbyrestaurants/ui/NearByRestaurantsApp.kt
+++ app/src/main/java/com/wolt/nearbyrestaurants/ui/NearByRestaurantsApp.kt
@@ -1,11 +1,11 @@
 package com.wolt.nearbyrestaurants.ui
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -34,7 +34,7 @@ fun NearByRestaurantsApp() {
                 )
             }
         ) { innerPadding: PaddingValues ->
-            Box(
+            Surface(
                 modifier = Modifier.padding(innerPadding)
             ) {
                 RestaurantsScreen()
